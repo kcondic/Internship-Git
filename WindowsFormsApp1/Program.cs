33d9be3afe0ec28c666aec8b6c040ad07eba419b
kcondic <kreso@dump.hr>
2018-11-24 19:12:56 +0100

    Added rebase dummy commit

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -18,6 +18,7 @@ namespace WindowsFormsApp1
             Console.WriteLine("Ovo je moja promjena");
             Console.WriteLine("Ovo je DUMP internship");
             Console.WriteLine("Ovo je promjena u branchu");
+            Console.WriteLine("Linija za rebase");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
